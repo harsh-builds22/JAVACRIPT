905a0290d7bcc73b18ae5c4484a992774a4d8e51
Harsh Sharma <harshsh2214@gmail.com>
2026-05-23 00:53:50 +0530

    api and json

--- API & Json/script.js
+++ API & Json/script.js
@@ -31,13 +31,50 @@ isMarried:false,
 
 
 // json example
-
-let json = `{
+   //  typeof(json)  is object
+ 
+let json = {
     "name":"harsh",
     "age":19,
     "city":"delhi",
     "isMarried":false
-}`
+}
+
+console.log(json);
+console.log(json.name);
+console.log(json.age);
+console.log(json.city);
+console.log(json.isMarried);
+
+  let json2 = [
+    {
+    "name":"harsh",
+    "age":19,
+    "city":"kota",
+    "isMarried":false    
+
+    },
+
+     {
+    "name":"ayan",
+    "age":19,
+    "city":"delhi",
+    "isMarried":false    
+
+    },
+
+     {
+    "name":"naitik",
+    "age":18,
+    "city":"jodhpur",
+    "isMarried":false    
+
+    }
+
+  ]
+
+
+
 
 // Number, string, boolean, null, array, object are data types in json
 
@@ -46,4 +83,105 @@ let json = `{
  //  it should be in comma separated values for multiple key value pairs.
 
 
+
+
+ //  json verson as string
+ let jsonString = `{
+  "name": "swati",
+  "age": 25,
+  "city": "mumbai",
+  "isMarried": true
+}`
  
+
+console.log(jsonString);  
+
+//  when u want to convert json string to json object, we use JSON.parse() method
+let jsonObject = JSON.parse(jsonString);
+console.log(jsonObject);
+console.log(jsonObject.name);
+console.log(jsonObject.age);
+console.log(jsonObject.city);
+console.log(jsonObject.isMarried);
+
+
+
+//  when u want to convert json object to json string, we use JSON.stringify() method
+
+let jsonString2 = JSON.stringify(jsonObject);
+console.log(jsonString2);
+console.log(typeof jsonString2); // string
+
+
+
+//  Practice question 
+
+let arr = [
+  {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  },
+  {
+    "id": 2,
+    "name": "Ervin Howell",
+    "username": "Antonette",
+    "email": "Shanna@melissa.tv",
+    "address": {
+      "street": "Victor Plains",
+      "suite": "Suite 879",
+      "city": "Wisokyburgh",
+      "zipcode": "90566-7771",
+      "geo": {
+        "lat": "-43.9509",
+        "lng": "-34.4618"
+      }
+    },
+    "phone": "010-692-6593 x09125",
+    "website": "anastasia.net",
+    "company": {
+      "name": "Deckow-Crist",
+      "catchPhrase": "Proactive didactic contingency",
+      "bs": "synergize scalable supply-chains"
+    }
+  }
+]
+
+
+console.log(arr[0].name);
+console.log(arr[0].address.city);
+console.log(arr[0].company.name);
+console.log(arr[1].name);
+console.log(arr[1].address.city);
+console.log(arr[1].company.name);
+
+
+
+
+
+
+//    API
+
+// API stands for Application Programming Interface, 
+// it is a set of rules and protocols for building and interacting with software applications.
+//  It allows different software applications to communicate with each other, it is a way for different software applications to interact with each other,
+//  it is a way for different software applications to share data and functionality with each other.
+
