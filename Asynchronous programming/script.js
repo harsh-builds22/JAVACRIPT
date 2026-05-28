ef9c714c44640e3314527c679aa4ee44977b89c8
Harsh Sharma <harshsh2214@gmail.com>
2026-05-28 23:31:57 +0530

    your message

--- Asynchronous programming/script.js
+++ Asynchronous programming/script.js
@@ -78,3 +78,127 @@ update          |  PUT/PATCH    | To update an existing data
 delete          |  DELETE       | To delete a data
 
 */
+
+
+
+  
+//  js is single threaded language.this means it does one thing at a time.
+// suppose u want to fetch data from server.it takes 2 seconds.if we wait nrmlly ,the whole app frzes.users cant click or scroll.
+    
+ //               Callback -----> Promises----> async await
+
+
+/* 
+
+  explain syncroous and asynchronous programming with example
+
+ 1.synchronous 
+   2.asynchronous            all synchronous code goes in main stack and asychronous in side stack,first all main stack code will excheuted first
+   3.synchronous              then asynchronous code will be executed when the main stack is empty and the "event loop" will check for any asynchronous code in the side stack and execute it.
+   4.synchronous             
+   5.asynchronous            
+   6.synchronous             
+                             
+*/                           
+
+
+
+ 
+
+
+//   Callbacks are function that are passed as arguments to other functions and are executed after a certain event or condition is met.
+//  They are commonly used in asynchronous programming to handle the results of asynchronous operations.
+ 
+
+//  callback vo function hota he jisko ham as a argument pass kar rhe hote he .
+
+function hod(name , callback){
+     callback(name);
+}
+    function printname(name){
+        console.log("hello " , name);
+    }
+
+
+hod("harsh sharma", printname);
+
+
+
+
+function calculate (a,b, callback1){
+    callback1(a,b);
+}
+
+
+function printsum(a,b){
+    console.log("sum is " , a+b);
+}
+
+function substract(a,b){
+    console.log("difference is " , a-b);
+}
+
+
+calculate(10,5, printsum);
+calculate(10,5, substract);
+
+
+
+
+function fetchData(processData){
+    setTimeout(() => {
+        const data = { name: "harsh sharma", age: 22 };
+        processData(data);
+    }, 2000);
+}
+
+function displayData(data){
+    console.log("Data received: ", data);
+}
+
+fetchData(displayData);
+
+
+
+//   example more
+
+
+
+function fetchData1(data2 , callback2){
+    setTimeout(() => {
+         console.log(data2);
+         callback2();
+    },3000);
+}
+
+
+console.log("fetching data...");
+
+/*
+ fetchData1('data 1   ', giveData);
+
+ fetchData1( giveData1);  
+
+ function giveData(){                            u can do it by this 
+     console.log("data 2 aa gya");
+     giveData1()
+ }
+
+ function  giveData1(){
+     console.log("data 3");
+    
+ }
+
+*/
+
+ fetchData1(' data 1', ()=>{
+      fetchData1('data 2', ()=>{                     //   best method to do it
+          fetchData1('data 3', ()=>{
+              console.log("all data fetched");
+          })
+        })
+    })
+
+
+
+    '='
